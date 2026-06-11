4f48be606ae32c4d3485b2a31b0f4e4ac2bf26ce
zhengtao358 <2560168219@qq.com>
2026-06-11 14:12:42 +0800

    init: 柏尔之源 摩洛哥手工阿甘油品牌官网首版
    - 4 个静态页面：首页 / 全部商品 / 关于我们 / 会员登录
    - 暖陶土 + 阿甘金配色，衬线宋体标题，三层阴影立体卡片
    - jQuery 1.7.2 + 原生 HTML/CSS

---
+++ images/柏尔之源 摩洛哥手工阿甘油_files/base.js
@@ -0,0 +1,214 @@
+﻿(function(){
+
+//顶端adver渐隐
+	
+function FadeAdver(args){
+	for(var i in args){
+			this[i] = args[i];	
+		}	
+		this.speed = args.speed ? args.speed : 5000;	//间隔时间默认5秒
+		this.sTime = args.sTime ? args.sTime : 500;	//渐进时间，默认1秒
+		this.load();
+		this.start();
+}
+
+FadeAdver.prototype ={
+	constructor : this,
+	load : function(){
+		var _this = this;
+		this.num = 0;	//计时器
+		this.mNum = this.num+1;	//轮播计时
+		this.len = this.divs.length;					
+		
+		//所有div设置absolute并排好index
+		this.divs.each(function(num){
+			var z_index = 50-num;
+			$(this).css({
+				"position" : "absolute",
+				"left" : 0,
+				"top" : 0,
+				"z-index" : z_index,
+				"display" : "none"	
+			})
+		});
+		
+		$(this.divs[0]).show();
+		
+		//所有div设置absolute并排好index
+		
+			
+		this.btns.each(function(num){
+			$(this).mouseover(function(){
+				_this.show(num);	
+				_this.stop();				
+			}).mouseout(function(){
+				_this.start();	
+			});	
+		});
+		
+		//左右按钮的使用
+		if(!!this.preBtn && !!this.nextBtn){
+			this.preBtn.css("z-index",60);
+			this.preBtn.click(function(){
+				var num = _this.num - 1;
+				if(num < 0){
+					num = _this.len-1;		
+				}	
+				_this.show(num);
+			});	
+			this.nextBtn.css("z-index",60);
+			this.nextBtn.click(function(){
+				var num = _this.num + 1;
+				if(num >= _this.len){
+					num = 0;		
+				}	
+				_this.show(num);
+			});	
+		}
+		
+		this.divs.each(function(num){
+			$(this).mouseover(function(){					
+				_this.stop();				
+			}).mouseout(function(){
+				_this.start();	
+			});	
+		});
+	},
+	show : function(num){
+		if(num == this.num) return;	//同一个返回
+		
+		
+		var _this = this;
+		this.flag  = false;	//关闭控制开关
+		this.btns.each(function(i){
+			if(i == num){
+				$(this).addClass("hover");	
+			}else{
+				$(this).removeClass("hover");	
+			}				
+		});
+				
+		$(this.divs[this.num]).fadeOut(this.sTime);	//旧的淡出
+						
+		$(this.divs[num]).fadeIn(_this.sTime);		//新的淡入
+		_this.num = num;	
+		_this.mNum = num+1;			
+				
+	},
+	start : function(){
+		var _this = this;					
+		this.interval = setInterval(function(){					
+			if(_this.mNum >= _this.len){
+				_this.mNum = 0;
+			}						
+			_this.show(_this.mNum);								
+		},this.speed);
+	},
+	stop : function(){
+		clearInterval(this.interval);
+	}	
+};
+
+window.FadeAdver = FadeAdver;
+//顶端adver	
+
+//新tabs切换
+	function tabs(handles,cons){
+		$(handles).each(function(num){
+	    	$(this).click(function(){    		
+	    		$(handles).removeClass("hover");
+	    		$(this).addClass("hover");
+	    		$(cons).removeClass("show");
+	    		$($(cons)[num]).addClass("show");
+	    	});
+	    });
+	}
+
+	window.tabs = tabs;
+
+
+
+	function tabs2(handles,cons){
+		$(handles).each(function(num){
+	    	$(this).mouseover(function(){    		
+	    		$(handles).removeClass("hover");
+	    		$(this).addClass("hover");
+	    		$(cons).removeClass("show");
+	    		$($(cons)[num]).addClass("show");
+	    	});
+	    });
+	}
+
+	window.tabs2 = tabs2;
+
+	//文字滚动类
+function TextSlider(args){
+	for(var i in args){
+		this[i] = args[i];	
+	}	
+	this.speed = args.speed ? args.speed : 50;
+	this.direction = args.direction ? args.direction : "top";
+	this.load();
+	this.start();
+}
+
+TextSlider.prototype = {
+	constructor : this,
+	load: function(){
+		var _this = this;		
+		
+		if(this.direction == "left"){//left的浮动处理
+			var child = this.inner.children();
+			childWidth = child.width() + parseInt(child.css("border-left-width")) + parseInt(child.css("border-right-width"))+parseInt(child.css("margin-left"))+parseInt(child.css("margin-right"));
+			this.inner.css({
+				"width" : childWidth*child.length,
+				"float" : "left"	
+			});
+			this.inner.parent().css("width",this.inner.width()*2);
+		}
+		
+		this.inner.clone(true).appendTo(this.inner.parent());
+		this.outer.hover(function(){
+			_this.stop();	
+		},function(){
+			_this.start();	
+		});
+	},
+	start : function(){
+		var _this = this;
+		
+		if(this.direction == "top"){
+			this.inter = setInterval(function(){
+				var _top = _this.outer.scrollTop()+1;
+				if(_top > _this.inner.height()){
+					_top = 0;	
+				}
+				_this.outer.scrollTop(_top);	
+			},this.speed);
+		}else{
+			this.inter = setInterval(function(){
+				var _left = _this.outer.scrollLeft()+1;
+				if(_left > _this.inner.width()){
+					_left = 0;	
+				}
+				_this.outer.scrollLeft(_left);	
+			},this.speed);	
+		}
+		
+	},
+	stop : function(){
+		if(this.inter){
+			clearInterval(this.inter);	
+		}
+	}	
+};
+
+window.TextSlider = TextSlider;
+//文字滚动类
+
+})();
+
+$(function(){
+
+
+});	
